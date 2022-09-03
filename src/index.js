73f04829568afdf19828dbd7d23da7a0efdead4f
tania <taniaosano15@gmail.com>
2022-09-03 11:58:06 +0300

    declare variable in javascript

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=edfd7aa4792af952873ca60f7f05e6a8&page=1'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+
+const form = document.querySelector('.form')
+const main = document.querySelector('.main')
+const movieHeader = document.getElementById('movie__header')
